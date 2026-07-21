53291506cfa81b2f6aa784848a3ab65f756c64f5
Amit Kumar <amitkumar4102004b@gmail.com>
2026-07-21 10:26:14 +0530

    added spring data jpa handson 6 exercixe

---
+++ Java FSE/Deepskilling/Spring_Data_JPA_with_Hibernate/1_spring_data_jpa_handson/Add_a_new_country/code/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,93 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+import com.cognizant.ormlearn.exception.CountryNotFoundException;
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
+    private static void testFindCountryByCode() {
+
+        LOGGER.info("Find Country By Code");
+
+        try {
+
+            Country country =
+                    countryService.findCountryByCode("IN");
+
+            System.out.println(country);
+
+        } catch (CountryNotFoundException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+    }
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(OrmLearnApplication.class, args);
+
+        countryService = context.getBean(CountryService.class);
+
+        testGetCountry();
+        testAddCountry();
+        testUpdateCountry();
+        testDeleteCountry();
+        testSearchCountry();
+        testFindCountryByCode();
+    }
+
+    private static void testGetCountry() {
+        LOGGER.info("Find Country by Code");
+        Country country = countryService.getCountry("IN");
+        System.out.println(country);
+    }
+
+    private static void testAddCountry() {
+        LOGGER.info("Add Country");
+
+        Country country = new Country("XX", "Test Country");
+        countryService.addCountry(country);
+
+        System.out.println(countryService.getCountry("XX"));
+    }
+
+    private static void testUpdateCountry() {
+        LOGGER.info("Update Country");
+
+        Country country = new Country("XX", "Updated Test Country");
+        countryService.updateCountry(country);
+
+        System.out.println(countryService.getCountry("XX"));
+    }
+
+    private static void testDeleteCountry() {
+        LOGGER.info("Delete Country");
+
+        countryService.deleteCountry("XX");
+
+        System.out.println(countryService.getCountry("XX"));
+    }
+
+    private static void testSearchCountry() {
+        LOGGER.info("Search Country");
+
+        List<Country> countries =
+                countryService.searchCountries("un");
+
+        countries.forEach(System.out::println);
+    }
+}
